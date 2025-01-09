20566d9da076dc7a320da14511d931671d835037
GabiCostaDev <113050825+GabiCostaDev@users.noreply.github.com>
2025-01-08 22:27:03 -0500

    Delete Week03 directory

--- Week03/Day01_CompLib/src/App.js
+++
@@ -1,7 +0,0 @@
-// import ButtonPage from './pages/ButtonPage'
-// import AccordionPage from './pages/AccordionPage'
-import ModalPage from './pages/ModalPage'
-
-export default function App() {
-  return <ModalPage />
-}
--- Week03/Homework/src/pages/ProgressBarPage.js
+++
@@ -1,9 +0,0 @@
-import ProgressBar from "../components/ProgressBar";
-
-export default function ProgressBarPage() {
-  return (
-    <>
-      <ProgressBar progress={75} duration={3000} />
-    </>
-  );
-}
